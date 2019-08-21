a6fe4a0ba322703db935858297ef1209152fb536
Krzysztof Podmokly <krzysztof.podmokly@gmail.com>
2019-08-21 09:55:38 +0200

    Changing Button component styling and folder structure

--- client/src/components/Translation/Translation.js
+++ client/src/components/Translation/Translation.js
@@ -1,18 +1,20 @@
 import React, { Fragment } from 'react';
 
 import classes from './Translation.module.css';
+import Button from '../UI/Button/Button';
 
-const Word = props => {
+const Translation = props => {
   return (
     <Fragment>
       <div className={classes.Wrapper}>
+        <div className={classes.Word}>der Pfirsich</div>
+      </div>
+      <Button>REVEAL TRANSLATION</Button>
+      <div className={classes.Wrapper}>
         <div className={classes.Translation}>Ich esse Pfirsich jeden Tag</div>
-        <div className={classes.Translation}>
-          Letztes mal, letze Woche habe Ich ein Pfirsich gegessen
-        </div>
       </div>
     </Fragment>
   );
 };
 
-export default Word;
+export default Translation;
--- client/src/containers/TranslationGenerator/TranslationGenerator.js
+++ client/src/containers/TranslationGenerator/TranslationGenerator.js
@@ -1,14 +1,10 @@
 import React, { Fragment } from 'react';
 
-import Word from '../../components/Word/Word';
 import Translation from '../../components/Translation/Translation';
 
 const TranslationGenerator = props => {
   return (
     <Fragment>
-      <section className='section-word'>
-        <Word />
-      </section>
       <section className='section-translation'>
         <Translation />
       </section>
