a2195188c1e5322f6949a1e676c0ba243d09ad0f
Krzysztof Podmokly <krzysztof.podmokly@gmail.com>
2019-08-22 15:25:16 +0200

    Adding custom form validation

--- client/src/components/Translation/Translation.js
+++ client/src/components/Translation/Translation.js
@@ -1,26 +1,34 @@
 import React, { Fragment } from 'react';
 
 import classes from './Translation.module.css';
+import globalClasses from '../../assets/styles/Global.module.css';
 import Button from '../UI/Button/Button';
 
 const Translation = props => {
-  console.log(props);
   return (
     <Fragment>
-      <div className={classes.Wrapper}>
+      <div className={globalClasses.Wrapper}>
         <div className={classes.Word}>der Pfirsich</div>
       </div>
       <Button>REVEAL TRANSLATION</Button>
-      <div className={[classes.Wrapper, classes.MarginTopMedium].join(' ')}>
-        <div className={[classes.Translation, classes.General].join(' ')}>
+      <div
+        className={[globalClasses.Wrapper, globalClasses.MarginTopMedium].join(
+          ' '
+        )}
+      >
+        <div className={[classes.Translation, globalClasses.General].join(' ')}>
           Brzoskwinia
         </div>
       </div>
-      <div className={[classes.Wrapper, classes.MarginTopSmall].join(' ')}>
-        <div className={[classes.Sentence, classes.General].join(' ')}>
+      <div
+        className={[globalClasses.Wrapper, globalClasses.MarginTopSmall].join(
+          ' '
+        )}
+      >
+        <div className={[classes.Sentence, globalClasses.General].join(' ')}>
           Ich esse Pfirsich jeden Tag
         </div>
-        <div className={[classes.Sentence, classes.General].join(' ')}>
+        <div className={[classes.Sentence, globalClasses.General].join(' ')}>
           Letztes mal, letzte Woche have Ich ein Pfirsich gegessen
         </div>
       </div>
