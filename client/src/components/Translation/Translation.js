f8b1efbc9cb97eb09cf8e6aae05a4ab47ac1b964
Krzysztof Podmokly <krzysztof.podmokly@gmail.com>
2019-08-23 14:51:22 +0200

    Adding fetchTranslationStart to manipulate loading state in Translation component

--- client/src/components/Translation/Translation.js
+++ client/src/components/Translation/Translation.js
@@ -1,54 +1,70 @@
-import React, { Fragment, useEffect } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 
 import classes from './Translation.module.css';
 import globalClasses from '../../assets/styles/Global.module.css';
 import Button from '../UI/Button/Button';
 import * as actions from '../../store/actions';
+import Spinner from '../UI/Spinner/Spinner';
 
-const Translation = ({ translation, fetchTranslationData }) => {
-  console.log(translation);
+const Translation = ({ translation, loading, error, fetchTranslationData }) => {
+  // check fetched object on page load, if it is empty show nothing
+  const renderContent =
+    Object.entries(translation).length !== 0 && !loading ? (
+      <Fragment>
+        <div className={globalClasses.Wrapper}>
+          <div className={classes.Word}>
+            {translation.article} {translation.word}
+          </div>
+        </div>
+        <div
+          className={[
+            globalClasses.Wrapper,
+            globalClasses.MarginTopMedium
+          ].join(' ')}
+        >
+          <div
+            className={[classes.Translation, globalClasses.General].join(' ')}
+          >
+            {translation.wordTranslated}
+          </div>
+        </div>
+        <div
+          className={[globalClasses.Wrapper, globalClasses.MarginTopSmall].join(
+            ' '
+          )}
+        >
+          {translation.sentences.map(sentence => (
+            <div
+              key={sentence._id}
+              className={[classes.Sentence, globalClasses.General].join(' ')}
+            >
+              {sentence.sentence}
+            </div>
+          ))}
+        </div>
+      </Fragment>
+    ) : null;
 
   return (
     <Fragment>
-      <div className={globalClasses.Wrapper}>
-        <div className={classes.Word}>der Pfirsich</div>
-      </div>
-      <Button clicked={fetchTranslationData}>REVEAL TRANSLATION</Button>
-      <div
-        className={[globalClasses.Wrapper, globalClasses.MarginTopMedium].join(
-          ' '
-        )}
-      >
-        <div className={[classes.Translation, globalClasses.General].join(' ')}>
-          Brzoskwinia
-        </div>
-      </div>
-      <div
-        className={[globalClasses.Wrapper, globalClasses.MarginTopSmall].join(
-          ' '
-        )}
-      >
-        <div className={[classes.Sentence, globalClasses.General].join(' ')}>
-          Ich esse Pfirsich jeden Tag
-        </div>
-        <div className={[classes.Sentence, globalClasses.General].join(' ')}>
-          Letztes mal, letzte Woche have Ich ein Pfirsich gegessen
-        </div>
-      </div>
+      <Button clicked={fetchTranslationData}>MAKE IT RANDOM!</Button>
+      {loading ? <Spinner /> : renderContent}
     </Fragment>
   );
 };
 
 const mapStateToProps = state => {
   return {
-    translation: state.translation.wordData
+    translation: state.translation.wordData,
+    loading: state.translation.loading,
+    error: state.translation.error
   };
 };
 
 const mapDispatchToProps = dispatch => {
   return {
-    fetchTranslationData: () => dispatch(actions.fetchTranslation())
+    fetchTranslationData: () => dispatch(actions.fetchTranslationSuccess())
   };
 };
 
