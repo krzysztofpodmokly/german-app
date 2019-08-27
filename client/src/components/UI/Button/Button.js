26c53c36a88fbaa69fa7a1abed5b83c8fa38ef8d
Krzysztof Podmokly <krzysztof.podmokly@gmail.com>
2019-08-27 14:59:18 +0200

    Adding fade in effect to button

--- client/src/components/UI/Button/Button.js
+++ client/src/components/UI/Button/Button.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import classes from './Button.module.css';
 import * as actions from '../../../store/actions';
