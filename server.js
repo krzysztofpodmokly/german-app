4fe5fdcfbfc9b050bb74e6a2f6116cbc56dfa8a8
Krzysztof <krzysztof.podmokly@gmail.com>
2019-09-01 14:08:42 +0200

    Prepared for deployment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connect = require('./config/db');
+const path = require('path');
 
 const app = express();
 connect();
@@ -8,6 +9,16 @@ connect();
 app.use(express.json({ extended: false }));
 
 app.use('/api/translations', require('./routes/api/translations'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.NODE_ENV === 'development' ? process.env.PORT : 5000;
 
