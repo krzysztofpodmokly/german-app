5e53a8f165012de352fb1904c22352197608afda
Krzysztof <krzysztof.podmokly@gmail.com>
2019-09-01 15:08:48 +0200

    Prepared for deployment

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-const PORT = process.env.NODE_ENV === 'development' ? process.env.PORT : 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log('App is running on port ' + PORT);
